5a0d7f172e0b7966fdf6272b6f92b36c66e3f0c5
gdstp <gabriel.teles.pinto@gmail.com>
2021-12-28 14:13:10 -0300

    feat: add mutation to create user

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 import { Context } from '../context';
 import User from '../schemas/User';
 
@@ -7,6 +7,16 @@ class UserResolver {
   @Query((returns) => [User])
   async find(@Ctx() ctx: Context) {
     return ctx.prisma.user.findMany();
+  }
+
+  @Mutation((returns) => User, { name: 'createUser' })
+  async create(
+    @Arg('username') username: string,
+    @Arg('password') password: string,
+    @Arg('name') name: string,
+    @Ctx() { prisma }: Context
+  ) {
+    return await prisma.user.create({ data: { username, name, password } });
   }
 }
 
